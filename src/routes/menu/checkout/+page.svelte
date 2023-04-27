e1b21d9dffe30573cfc1ddbbc97d6e2fd61767d3
xcheesee <lucas121520@gmail.com>
2023-04-26 22:33:40 -0300

    implementado redirecionamento apos o envio de um pedido

--- src/routes/menu/checkout/+page.svelte
+++ src/routes/menu/checkout/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { redirect } from "@sveltejs/kit";
 	import CheckoutItemCard from "../../../components/CheckoutItemCard.svelte";
 	import { carrinhoStore } from "../../../utils/stores";
+	import { goto } from "$app/navigation";
 
   function getTotal(val: string, multiplier: number): number {
 		const formattedVal = +val.replace(",", ".")
@@ -17,7 +19,10 @@
 <div class="flex justify-center">
 	<div class="flex flex-col gap-4 pt-8 lg:w-[1024px]">
 		{#if Object.keys($carrinhoStore).length === 0}
-			<div> Carrinho Vazio! </div>
+			<div class="text-center"> Carrinho Vazio! </div>
+			<div class="flex justify-center mt-8">
+			  <button class="btn variant-filled-primary" on:click={() => goto("/menu")}> Voltar ao Menu</button>
+			</div>
 		{:else}
 			{#each Object.values($carrinhoStore) as item}
 				<CheckoutItemCard item={item} calculaTotal={calculaTotal} />
@@ -41,7 +46,14 @@
 						})
 					})
 					
-					if(res.ok) alert("Pedido enviado com sucesso!")
+					if(!res.ok) {
+						alert("Houve um erro no envio :(")
+					}
+
+					if(confirm("Pedido enviado com sucesso!")) {
+						$carrinhoStore = {}
+						goto("/menu")
+					}
 				}}>Finalizar Compra</button>
 			</div>
 		{/if}
