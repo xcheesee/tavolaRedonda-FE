f9f77b28fa2ef6f2ce36f75bd407736186d8b26e
xcheesee <lucas121520@gmail.com>
2023-04-27 20:39:36 -0300

    adicionado funcao de limpar carrinho

--- src/routes/menu/checkout/+page.svelte
+++ src/routes/menu/checkout/+page.svelte
@@ -6,7 +6,7 @@
 	import { goto } from "$app/navigation";
 	import { error } from "@sveltejs/kit";
 
-  function getTotal(val: string, multiplier: number): number {
+	function getTotal(val: string, multiplier: number): number {
 		const formattedVal = +val.replace(",", ".")
 		const total = formattedVal * multiplier
 		return total
@@ -32,33 +32,35 @@
 		{#if Object.keys($carrinhoStore).length === 0}
 			<div class="text-center"> Carrinho Vazio! </div>
 			<div class="flex justify-center mt-8">
-			  <button class="btn variant-filled-primary" on:click={() => goto("/menu")}> Voltar ao Menu</button>
+				<button class="btn variant-filled-primary" on:click={() => goto("/menu")}> Voltar ao Menu</button>
 			</div>
 		{:else}
 			{#each Object.values($carrinhoStore) as item}
 				<CheckoutItemCard item={item} />
 			{/each}
 			<div class="flex justify-between">
-			  <div> Total: R${current} </div>
+				<div> Total: R${current} </div>
+				<div>
+				<button class="btn variant-ghost-warning" on:click={() => $carrinhoStore = {}}> Limpar Carrinho </button>
 				<button class="btn variant-ghost-success" on:click={async () => {
 					const res = await fetch("http://127.0.0.1:8000/api/pedidos", {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json"
-						},
-						body: JSON.stringify({
-							cliente_id: 1,
-							Status: 0,
-							forma_pagamento: "Dinheiro",
-							itens: Object.values($carrinhoStore).map(val => ({
-								produto_id: val.id, 
-								quantidade: val.qtd,
-							}))
-						})
+					method: "POST",
+					headers: {
+					"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+					cliente_id: 1,
+					Status: 0,
+					forma_pagamento: "Dinheiro",
+					itens: Object.values($carrinhoStore).map(val => ({
+					produto_id: val.id, 
+					quantidade: val.qtd,
+					}))
 					})
-					
+					})
+
 					if(!res.ok) {
-						return toastStore.trigger(errorToast)
+					return toastStore.trigger(errorToast)
 					}
 
 					$carrinhoStore = {}
@@ -66,6 +68,7 @@
 
 					return goto("/menu")
 				}}>Finalizar Compra</button>
+																																																																																																																																																																																										    </div>
 			</div>
 		{/if}
 	</div>
